ff027bf0f80371cb9dc4b206e5830f92baa53633
Vyacheslav Panchenko <vyacheslav.panchenko@valor-software.com>
2016-05-18 19:31:57 +0300

    feat(update-dataset): update dataset functionality based on git hash commits diff, diff to files

--- steps/flow-import-dataset-update-hash-to.js
+++ steps/flow-import-dataset-update-hash-to.js
@@ -26,6 +26,7 @@ require('shelljs/global');
 
 const holder = require('./../model/value-holder');
 const daff = require('daff');
+const fs = require('fs');
 
 
 step.prototype.process = function (inputValue) {
@@ -46,8 +47,12 @@ step.prototype.process = function (inputValue) {
   var resultGitDiff = exec(commandGitDiff, {silent: true}).stdout;
 
   var gitDiffFileList = resultGitDiff.split("\n").filter(function(value){
-    return !!value;
+    return !!value && value.indexOf(".csv") != -1;
   });
+
+  console.log("gitDiffFileList:", gitDiffFileList);
+
+  var dataRequest = {};
 
   gitDiffFileList.forEach(function(fileName, index){
 
@@ -67,15 +72,165 @@ step.prototype.process = function (inputValue) {
 
     var flags = new daff.CompareFlags();
     flags.show_unchanged = true;
+    flags.show_unchanged_columns = true;
+    flags.always_show_header = true;
 
     var highlighter = new daff.TableDiff(filesDiff, flags);
     highlighter.hilite(diffResult);
 
-    console.log("****************");
-    console.log(diffResult);
-    console.log("****************");
+    fs.writeFileSync("./requests/diff--" + fileName + ".json", JSON.stringify(diffResult));
 
-    sleep(10000);
+    /* Prepare Data Structure */
+
+    var fileDiffData = {
+
+      "header": {
+        "create": [],
+        "remove": [],
+        "update": []
+      },
+
+      "body": {
+        "create": [],
+        "remove": [],
+        "update": []
+      }
+
+    };
+
+
+
+    /* Slice Groupd of Changes */
+
+    var firsDiffRow = diffResult.shift();
+    var diffResultHeader = [];
+    var diffResultColumns = [];
+
+    if(firsDiffRow[0] == '!') {
+
+      // [ '!', '', '(old_column)', '+++', '---' ],
+      diffResultHeader = firsDiffRow;
+      // [ '@@', 'city', 'name', 'country' ],
+      diffResultColumns = diffResult.shift();
+
+      if(diffResultHeader[0] == "!") {
+
+        diffResultHeader.shift();
+        diffResultHeader.forEach(function(value, index){
+
+          if(value != '') {
+
+            if (value == '+++') {
+              // added
+              fileDiffData.header.create.push(diffResultColumns[index + 1]);
+            } else if (value == '---') {
+              // removed
+              fileDiffData.header.remove.push(diffResultColumns[index + 1]);
+            } else {
+              // modified
+              var oldColumn = value.substring(1, value.length - 1);
+              fileDiffData.header.update.push({
+                oldColumn: diffResultColumns[index + 1]
+              });
+            }
+          }
+
+        });
+      }      
+
+    } else {
+      // [ '@@', 'city', 'name', 'country' ],
+      diffResultColumns = firsDiffRow;
+    }
+
+    var diffResultGidField;
+    if(diffResultColumns[0] == "@@") {
+      diffResultColumns.shift();
+      diffResultGidField = diffResultColumns[0];
+    }
+
+
+    if(diffResult.length) {
+
+      diffResult.forEach(function(value, index){
+
+        // simple-way, collect all data (mean full row) for update
+
+        var modificationType = value.shift();
+
+        if(modificationType != '') {
+
+          if (modificationType == '+++') {
+
+            // added
+            var dataRow = {};
+            diffResultColumns.forEach(function(columnValue, columnIndex){
+              if(fileDiffData.header.remove.indexOf(columnValue) == -1) {
+                // ready columns
+                dataRow[columnValue] = value[columnIndex];
+              }
+            });
+
+            if(dataRow) {
+              fileDiffData.body.create.push(dataRow);
+            }
+
+          } else if (modificationType == '---') {
+
+            // removed
+            var dataRowRemoved = {};
+            dataRowRemoved[diffResultGidField] = value[0];
+            fileDiffData.body.remove.push(dataRowRemoved);
+
+          } else if (modificationType == '+') {
+
+            // updated, only added columns
+            var dataRow = {};
+            diffResultHeader.forEach(function(columnValue, columnIndex) {
+              if (columnValue == '+++') {
+                var columnKey = diffResultColumns[columnIndex];
+                dataRow[columnKey] = value[columnIndex];
+              }
+            });
+
+            var dataRowUpdated = {};
+            dataRowUpdated["gid"] = diffResultGidField;
+            dataRowUpdated[diffResultGidField] = value[0];
+            dataRowUpdated["data"] = dataRow;
+
+            fileDiffData.body.update.push(dataRowUpdated);
+
+          } else if (modificationType == '->') {
+
+            // updated
+
+            /*
+            value.forEach(function(valueCell, indexCell){
+              var modificationSeparatorPosition = valueCell.indexOf('->');
+              var readyValueCell = valueCell;
+              if(modificationType == '->' && modificationSeparatorPosition != -1) {
+                readyValueCell = valueCell.substring(modificationSeparatorPosition + 2);
+              }
+              fileDiffData.header.create.push(diffResultColumns[index + 1]);
+            });
+            */
+
+          }
+        }
+
+      });
+
+    }
+
+
+
+    // Structure :: Create
+
+    dataRequest[fileName] = fileDiffData;
+
+    //console.log("diffResult", diffResult);
+    fs.writeFileSync("./requests/" + fileName + ".json", JSON.stringify(fileDiffData));
+
   });
 
   // git diff 9f2e28d 5a4cacb --name-only
